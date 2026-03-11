f55b690a3bca3cbdefe43c92308cfeeba59a01a6
xsenente <xavier.senente@gmail.com>
2026-03-11 11:12:26 +0100

    feat: modifier la configuration pour passer à un rendu statique et supprimer le pré-rendu des pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://huedata.fr",
-  output: "server",
+  output: "static",
   adapter: netlify(),
 
   vite: {
