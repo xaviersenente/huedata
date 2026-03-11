252f2eb9dd274b88587326d13c2dee2a38e3679c
xsenente <xavier.senente@gmail.com>
2026-03-11 10:39:42 +0100

    feat: mettre à jour la configuration pour utiliser le rendu côté serveur et ajouter le pré-rendu sur plusieurs pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://huedata.fr",
-  output: "static",
+  output: "server",
   adapter: netlify(),
 
   vite: {
